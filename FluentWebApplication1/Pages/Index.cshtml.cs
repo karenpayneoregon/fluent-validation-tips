f205943513fab207cc9a4b0b9bc9ba79cc72a657
Karen Payne <paynekaren@comcast.net>
2025-10-01 08:21:31 -0700

    Clean-up

--- FluentWebApplication1/Pages/Index.cshtml.cs
+++ FluentWebApplication1/Pages/Index.cshtml.cs
@@ -1,12 +1,8 @@
 ﻿using FluentValidation;
 using FluentValidation.AspNetCore;
-using FluentValidation.Results;
 using FluentWebApplication1.Models;
-using FluentWebApplication1.Validators;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Serilog;
-using System.ComponentModel.DataAnnotations;
 
 namespace FluentWebApplication1.Pages;
 public class IndexModel(IValidator<Customer> validator) : PageModel
@@ -19,40 +15,25 @@ public class IndexModel(IValidator<Customer> validator) : PageModel
         Customer = new Customer();
         return Page();
     }
-
-
-
+    
     public PageResult OnPostSubmit()
     {
         // Perform validation
         var result = validator.Validate(Customer);
         if (!result.IsValid)
         {
+            // not for production use
             foreach (var error in result.Errors)
             {
                 Console.WriteLine(error.ErrorMessage);
             }
+            
             result.AddToModelState(ModelState);
             return Page();
         }
 
-        // If valid, proceed with your logic
+        // If valid, proceed 
         return Page();
     }
-
-    //public IActionResult OnPost()
-    //{
-    //    var result =  _validator.Validate(Customer);
-    //    if (!result.IsValid)
-    //    {
-
-    //        result.AddToModelState(ModelState);
-    //        return Page();
-
-    //    }
-
-
-    //    return RedirectToPage("./List");
-    //}
 
 }
