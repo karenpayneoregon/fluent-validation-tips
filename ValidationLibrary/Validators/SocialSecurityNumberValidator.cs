03381d1fc2554a06b59d468e6b15885dfd32237d
Karen Payne <paynekaren@comcast.net>
2025-10-24 08:43:48 -0700

    Add XML docs to SocialSecurityNumberValidator class
    by Karen

--- ValidationLibrary/Validators/SocialSecurityNumberValidator.cs
+++ ValidationLibrary/Validators/SocialSecurityNumberValidator.cs
@@ -3,6 +3,13 @@
 namespace ValidationLibrary.Validators;
 public class SocialSecurityNumberValidator : AbstractValidator<string>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SocialSecurityNumberValidator"/> class.
+    /// </summary>
+    /// <remarks>
+    /// This validator ensures that the provided Social Security Number (SSN) is not empty
+    /// and adheres to a valid SSN format using the <see cref="SpecialValidation.IsValidSocialSecurityNumber"/> method.
+    /// </remarks>
     public SocialSecurityNumberValidator()
     {
         var specialValidation = new SpecialValidation();
