4c155e48e2ebe3240a1c0642d1d40e6f5880af2e
Karen Payne <paynekaren@comcast.net>
2025-10-24 08:42:33 -0700

    Add Bootstrap Icons and SSN Validator
    Added Bootstrap Icons to `Index.cshtml` for enhanced UI with icon-based action buttons (Edit, Details, Delete). Updated buttons to include icons and tooltips for better accessibility. Commented out the previous button style for potential reuse.
    
    Introduced `SocialSecurityNumberValidator` in `SocialSecurityNumberValidator.cs` using FluentValidation. Added rules to ensure SSN is not empty and adheres to a valid format using a custom validation method.
    
    by Karen

---
+++ ValidationLibrary/Validators/SocialSecurityNumberValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ValidationLibrary.Validators;
+public class SocialSecurityNumberValidator : AbstractValidator<string>
+{
+    public SocialSecurityNumberValidator()
+    {
+        var specialValidation = new SpecialValidation();
+        RuleFor(x => x)
+            .NotEmpty().WithMessage("Social Security Number is required.")
+            .Must(specialValidation.IsValidSocialSecurityNumber)
+            .WithMessage("Invalid Social Security Number format.");
+    }
+}
